a889437d8091597c1387c78905f3aa6c213fc579
Marah <marah.bossi.jed.k1@tuwaiq.edu.sa>
2021-10-18 02:04:04 +0300

    solved

--- app/src/main/java/com/shady/hw_week04_day01_recyclerviewiml/MainActivity.kt
+++ app/src/main/java/com/shady/hw_week04_day01_recyclerviewiml/MainActivity.kt
@@ -2,6 +2,7 @@ package com.shady.hw_week04_day01_recyclerviewiml
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.marah.recycleviewapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
